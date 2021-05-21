b8ac04f48ef9024aefb6c349a5f51f55926e7579
aoner <aoners@gmail.com>
2021-05-21 10:45:15 +0800

    Update imgkit.rb

--- lib/imgkit/imgkit.rb
+++ lib/imgkit/imgkit.rb
@@ -107,13 +107,13 @@ class IMGKit
   end
 
   def to_img(format = nil, path = nil)
-    append_stylesheets
-    append_javascripts
+#     append_stylesheets
+#     append_javascripts
     set_format(format)
 
     opts = @source.html? ? {:stdin_data => @source.to_s} : {}
     result, stderr = capture3(*(command(path) + [opts]))
-    result.force_encoding("ASCII-8BIT") if result.respond_to? :force_encoding
+#     result.force_encoding("ASCII-8BIT") if result.respond_to? :force_encoding
     raise CommandFailedError.new(command.join(' '), stderr) if path.nil? and result.size == 0
     result
   end
